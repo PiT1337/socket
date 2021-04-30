4ee3a058fa284cad43d85408e239ae98205cf521
PiT1337 <78843942+PiT1337@users.noreply.github.com>
2021-04-30 11:50:52 +0200

    socket scheletro

--- socket/socket/MainWindow.xaml.cs
+++ socket/socket/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +11,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//aggiunta delle seguenti librerie
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace socket
 {
@@ -23,6 +27,77 @@ namespace socket
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnCreaSocket_Click(object sender, RoutedEventArgs e)
+        {
+            IPEndPoint sourceSocket = new IPEndPoint(IPAddress.Parse("10.73.0.20"),56000);
+
+
+            
+            btnInvia.IsEnabled = true;
+            Thread ricezione = new Thread(new ParameterizedThreadStart(SocketReceive));
+
+
+            ricezione.Start(sourceSocket);
+
+
+        }
+        
+        private void btnInvia_Click(object sender, RoutedEventArgs e)
+        {//aggiunre controlli sul contenuto delle textbox
+            string ipaddress = txtindirizzoIp.Text;
+            int port =int.Parse( txtport.Text);
+            SocketSend(IPAddress.Parse(ipaddress), port, txtmessage.Text);
+
+
+
+
+        }
+        public async void SocketReceive(object socksource) 
+        {
+            IPEndPoint ipendp = (IPEndPoint)socksource;
+            Socket t = new Socket(ipendp.AddressFamily,SocketType.Dgram,ProtocolType.Udp);
+            t.Bind(ipendp);
+
+            Byte[] byteRicevti = new Byte[256];
+
+            string message;
+            int contaCaratteri = 0;
+
+            await Task.Run(() =>
+            {
+                while(true)
+                {
+                    if (t.Available > 0)
+                    {
+                        message = "";
+
+                        contaCaratteri = t.Receive(byteRicevti,byteRicevti.Length,0);
+                        message += Encoding.ASCII.GetString(byteRicevti, 0, contaCaratteri);
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblRicevi.Content = message;
+                        }));
+                    }
+                }
+            });
+
+        }
+
+
+
+        public void SocketSend(IPAddress dest,int destport,string message)
+        {
+            Byte[] byteTnviati = Encoding.ASCII.GetBytes(message);
+
+            Socket s = new Socket(dest.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint remote_endpoint = new IPEndPoint(dest, destport);
+
+            s.SendTo(byteTnviati, remote_endpoint);
+
+
         }
     }
 }
