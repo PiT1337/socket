5302313d6a80d314bca09bd9d0f64e5366ba0b89
PiT1337 <78843942+PiT1337@users.noreply.github.com>
2021-06-01 19:03:56 +0200

    fix errore ipv6
    ricerca del ipv4 tra i vari ip del systema

---
+++ socket/socket/socket/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//aggiunta delle seguenti librerie
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace socket
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //variabile per aggirare un errore
+        int startscimmia = 0;
+        
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        } 
+        private void btnCreaSocket_Click(object sender, RoutedEventArgs e)
+        {
+ 
+            //scimmia forte insieme (gestione del errore del creare 2 socket)
+            if (startscimmia==0) 
+            {
+                //visione a video del proprio ip nella label 
+                lbloutput.Content= "il tuo ip è :" + GetLocalIPAddress();
+                //avvio del Thread
+                Thread ricezione = new Thread(new ParameterizedThreadStart(SocketReceive));
+                ricezione.Start(new IPEndPoint(IPAddress.Parse(GetLocalIPAddress()), 56000));
+                startscimmia++;
+            }
+            //attivazione del altro bottone
+            btnInvia.IsEnabled = true;
+        }
+        
+        private void btnInvia_Click(object sender, RoutedEventArgs e)
+        {
+            //aggiunre controlli sul contenuto delle textbox
+            IPAddress provaip;
+            int provaint;
+            //controllo del input delle textbox
+            if (IPAddress.TryParse(txtindirizzoIp.Text, out provaip) && int.TryParse(txtport.Text, out provaint) && provaint > 0 && provaint < 65536) 
+            { 
+                SocketSend(IPAddress.Parse(txtindirizzoIp.Text), int.Parse(txtport.Text) , txtmessage.Text);
+            }
+            else
+            {
+                MessageBox.Show("errore nel inserimento dei campi ip-address o della porta");
+            }
+        }
+        public async void SocketReceive(object socksource) 
+        {
+            IPEndPoint ipendp = (IPEndPoint)socksource;
+            Socket t = new Socket(ipendp.AddressFamily,SocketType.Dgram,ProtocolType.Udp);
+            t.Bind(ipendp);
+            //max Byte per messaggio
+            Byte[] byteRicevti = new Byte[256];
+
+            string message;
+            int contaCaratteri = 0;
+
+            await Task.Run(() =>
+            {
+                while(true)
+                {
+                    if (t.Available > 0)
+                    {
+                        message = "";
+                        contaCaratteri = t.Receive(byteRicevti,byteRicevti.Length,0);
+                        message += Encoding.ASCII.GetString(byteRicevti, 0, contaCaratteri);
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblRicevi.Content = message;
+                        }));
+                    }
+                }
+            });
+
+        }
+        public void SocketSend(IPAddress dest,int destport,string message)
+        {
+            Byte[] byteTnviati = Encoding.ASCII.GetBytes(message);
+
+            Socket s = new Socket(dest.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint remote_endpoint = new IPEndPoint(dest, destport);
+
+            s.SendTo(byteTnviati, remote_endpoint);
+
+
+        }
+    }
+}
